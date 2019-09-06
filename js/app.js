e5ccb2d8b17e0dca8e9762163cb0a988ebc2ccba
Alex Woods <alex.woods89@hotmail.co.uk>
2019-09-06 14:23:14 +0100

    added radio button

--- js/app.js
+++ js/app.js
@@ -7,17 +7,13 @@ document.addEventListener('DOMContentLoaded', () =>{
 })
 
 const handleNewItemFormSubmit = function(event){
-    console.log(event);
     event.preventDefault()
 const parentElement = document.querySelector('ul')
-console.log(parentElement)
 const childElement = document.createElement('p')
-childElement.textContent = `${event.target.name.value}, ${event.target.team.value}, ${event.target.position.value}`
-console.log(childElement)
+childElement.textContent = `${event.target.name.value}, ${event.target.team.value}, ${event.target.position.value}, ${event.target.available.value}`
 parentElement.appendChild(childElement)
 }
 const handleDeleteButtonClick = function(event){
-    console.log(event)
 const sportstarList = document.querySelector('ul')
 sportstarList.innerHTML = ''
 
